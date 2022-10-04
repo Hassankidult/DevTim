3d619fe6c49a18ea1121852aeaad0bf18f99c843
Hassankidult <hassanaliyu33@gmail.com>
2022-10-04 13:52:18 +0100

    Closing navbar

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+//Mobile Navigation
 const headerEl = document.querySelector(".header");
 const mobileNabEl = document.querySelector(".mobile-nav-bar");
 
 mobileNabEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
+});
+
+//Closing navbar
+const links = document.querySelectorAll(".link");
+
+links.forEach(function (link) {
+  link.addEventListener("click", function () {
+    headerEl.classList.toggle("nav-open");
+  });
 });
